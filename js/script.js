f401722ab80fd5d74e68ba7f7dc6d1eb2ac6be80
Mehedi Hasan <mehedihasan6002@gmail.com>
2026-05-17 21:48:28 +0600

    redesigned

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ function handleContactForm(event) {
   event.target.reset();
   return false;
 }
+
+function handleVolunteerForm(event) {
+  event.preventDefault();
+  const volunteerModal = bootstrap.Modal.getInstance(
+    document.getElementById('volunteerModal')
+  );
+  volunteerModal.hide();
+
+  const successModal = new bootstrap.Modal(
+    document.getElementById('successModal')
+  );
+  successModal.show();
+  event.target.reset();
+  return false;
+}
